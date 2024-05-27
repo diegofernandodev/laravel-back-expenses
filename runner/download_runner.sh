66ff72c8cdae705b8ff28c0f55ac073e65159c55
diegofernandodev <dieferv@gmail.com>
2024-05-27 13:54:46 -0500

    se modificado los archivos del directorio runner y funciona

---
+++ runner/download_runner.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Descargar y configurar el runner de GitHub
+RUNNER_VERSION="2.316.1"
+RUNNER_ARCHIVE="actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz"
+RUNNER_URL="https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/${RUNNER_ARCHIVE}"
+
+# Crear el directorio para el runner
+mkdir -p /runner/actions-runner
+cd /runner/actions-runner
+
+# Descargar el runner
+curl -o ${RUNNER_ARCHIVE} -L ${RUNNER_URL}
+echo "d62de2400eeeacd195db91e2ff011bfb646cd5d85545e81d8f78c436183e09a8  ${RUNNER_ARCHIVE}" | shasum -a 256 -c
+
+# Extraer el runner
+tar xzf ./${RUNNER_ARCHIVE}
+
+# Eliminar el archivo tar
+rm ./${RUNNER_ARCHIVE}
--- runner/entrypoint.sh
+++ runner/entrypoint.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-# Variables necesarias
-URL=${REPO_URL}
-TOKEN=${RUNNER_TOKEN}
+# Asegúrate de que las variables de entorno están definidas
+if [ -z "$REPO_URL" ] || [ -z "$RUNNER_TOKEN" ]; then
+  echo "ERROR: REPO_URL o RUNNER_TOKEN no están definidos"
+  exit 1
+fi
+
+# Cambiar al directorio del runner
+cd /runner/actions-runner || exit
 
 # Configura el runner
-./config.sh --url $URL --token $TOKEN --unattended --replace
+./config.sh --url "$REPO_URL" --token "$RUNNER_TOKEN" --unattended --replace
 
-# Inicia el runner
-./run.sh
+# Definir función de limpieza para eliminar el runner al finalizar
+cleanup() {
+  echo "Removing runner..."
+  ./config.sh remove --unattended --token "$RUNNER_TOKEN"
+}
+
+# Capturar señales de terminación para ejecutar la limpieza
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+# Iniciar el runner
+./run.sh & wait $!
